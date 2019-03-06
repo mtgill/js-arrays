1d8780dab5a9e78d634f99c12bac32f808b00d2d
Matt Gill <mtgill90@outlook.com>
2019-03-05 21:26:41 -0600

    assignment cards should work

--- main.js
+++ main.js
@@ -34,6 +34,84 @@ const buildDinosaurs = () => {
 };
 
 
+
+const assignments = [
+    {
+        title: 'product cards',
+        dueDate: '03/05/2019',
+        topic: 'HTML/CSS',
+        notes: 'Use flexbox',
+        assignmentUrl: 'www.google.com',
+    },
+    {
+        title: 'assignment2',
+        dueDate: '04/11/2019',
+        topic: 'HTML/CSS',
+        notes: 'Use flexbox',
+        assignmentUrl: 'www.google.com',
+    },
+    {
+        title: 'assignment3',
+        dueDate: '05/05/2019',
+        topic: 'HTML/CSS',
+        notes: 'Use flexbox',
+        assignmentUrl: 'www.google.com',
+    },
+    {
+        title: 'assignment4',
+        dueDate: '06/05/2019',
+        topic: 'HTML/CSS',
+        notes: 'Use flexbox',
+        assignmentUrl: 'www.google.com',
+    },
+    {
+        title: 'assignment5',
+        dueDate: '07/05/2019',
+        topic: 'HTML/CSS',
+        notes: 'Use flexbox',
+        assignmentUrl: 'www.google.com',
+    },
+];
+
+//total of 5 assignments 
+//make a function called buildAssignmentCards - loop over the assignments and make a domString
+// reuse printToDom function to display the domString 
+
+// top - title area with border bottom
+// Due date - in bold
+// Link to assignment
+// topic
+//notes 
+
+
+const buildCard = () => {
+    let domString = '';
+    for(let i = 0; i < assignments.length; i++ ){
+        
+        
+        domString += `<div class='card'>`;
+        domString += `<div><div class='title'><h2>${assignments[i].title}</h2></div>`;
+        domString += `<h2>${assignments[i].dueDate}</h2>`;
+        domString += `<h3><a href=${assignments[i].assignmentUrl}>Get the assignment here</a></h3>`;
+        domString += `<h3>Topic: ${assignments[i].topic}</h3>`;
+        domString += `<h3>Notes: ${assignments[i].notes}</h3>`;
+        domString += `</div>`; 
+        domString += `</div>`; 
+        
+
+    }
+
+    printToDom('cards', domString);
+
+};
+
+
+
+buildCard();
+
+
+
+
 const init = () => {
     buildDinosaurs();
 };
